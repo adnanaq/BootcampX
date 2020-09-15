325a6b5f47fb31d9245e0aced827b1a5bc261edb
Dani <dreamboy.dani@gmail.com>
2020-09-15 12:47:19 -0600

    Add queries no 4

---
+++ 4_queries/2_students_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT count(assistance_requests.*) AS total_assistances, students.name
+FROM students
+  JOIN assistance_requests
+  ON students.id = assistance_requests.student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
